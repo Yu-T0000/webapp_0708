261385642df08a69b0c8c09496563de091564274
Yu-T0000 <chiave.yu@gmail.com>
2023-07-08 19:05:18 +0900

    no message

--- python/counter_sample.py
+++ python/counter_sample.py
@@ -1,11 +1,25 @@
 import streamlit as st
 
+_ = ''' # なんか変数化しないとコメントアウトされないっぽい
 st.title("Counter Example")
-if "count" not in st.session_state: # session_stateがcountを保持してない時
+if "count" not in st.session_state: # session_stateがcountを保持してない時(初期化対策)
     st.session_state.count = 0 # countに0代入
 
-increment = st.button("Increment") # 「Increment」ボタン
+increment = st.button("Increment") # 「Increment」ボタン 押されたタイミングでincrementにTrue
 if increment:
     st.session_state.count += 1 # countに+1
 
 st.write("Count = ", st.session_state.count) # 書き込み
+
+'''
+
+st.title("Counter Example using Callbacks") # コールバック関数を使うパターン
+if "count" not in st.session_state:
+    st.session_state.count = 0
+
+def increment_counter():    # 足し算するよ
+    st.session_state.count += 1
+
+st.button("Increment", on_click=increment_counter) # 押されたらincrement_counter()実行してね
+
+st.write("Count = ", st.session_state.count)
---
+++ python/text_input_sample.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.title("InputText using Callbacks")
+
+st.text_input("Message", key="text_input") # 入力欄の名前(ラベル),多分ここでst.session_state["text_input"]に"無"が作られてる
+
+def change_value():
+    st.session_state["text_input"] = "Hello, World" # st.session_state["text_input"]をHello Worldに設定
+
+
+st.button("Click", on_click=change_value) # ボタン押されたら真っ先にchange_valueを実行してね
+
+
+_ = '''
+#StreamlitAPIExceptionエラーが起きる
+
+st.text_input("Message", key="text_input")
+
+
+if st.button("Click"):
+    st.session_state["text_input"] = "Hello, World"
+
+ボタンが押される/入力されると上から処理をし直すので、
+まず最初にst.text_input("Message", key="text_input")でst.session_state["text_input"]が無を取得
+設定後の値変更はできない(st.session_state.text_input cannot be modified after 
+the widget with key text_input is instantiated.)のでエラーが起きる。
+
+コールバック使ってる方は
+ボタン押されてない：
+st.text_input("Message", key="text_input")でst.session_state["text_input"]が設定される
+
+ボタン押された：
+最初にst.session_state["text_input"] = "Hello, World"でst.session_state["text_input"]の値が設定される
+↓
+st.text_input("Message", key="text_input")でst.session_stateのtext_inputから値を持ってくる
+
+'''
