821d549f5b7220360bf0ec9460e35fd0c418c951
Yu-T0000 <chiave.yu@gmail.com>
2023-07-19 21:58:13 +0900

    no message

--- EC/yaoya/services/mock_db.py
+++ EC/yaoya/services/mock_db.py
@@ -1,7 +1,9 @@
 from collections.abc import Generator
 from pathlib import Path
-import dataset
 from contextlib import contextmanager
+
+import dataset
+from tinydb import TinyDB
 
 _ = '''
 「MockDB は dataset の Wrapper です。
@@ -16,11 +18,18 @@ class MockDB:
 
     @contextmanager
     def connect(self) -> Generator[dataset.Database, None, None]:
-        db = dataset.connect(self._dbname)
-        db.begin()
+        db = dataset.connect(self._dbname)  #SQLiteデータベースに接続して結果をdbに代入
+        db.begin()  #データベーストランザクション開始
         try:
             yield db
             db.commit()
         except Exception as e:
-            db.rollback()
-            raise e
+            db.rollback()   #様子がおかしかったら変更破棄
+            raise e #デバッグ用？
+
+dbpath = Path("sample.db")
+mock_db = MockDB(dbpath)    #sample.dbを参照するインスタンス
+
+with mock_db.connect() as db:
+    table: dataset.Table = db["samples"]
+    print(table.find_one(id= "001"))
